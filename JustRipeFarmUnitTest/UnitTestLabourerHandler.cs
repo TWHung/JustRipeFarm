cbb1543fa3af00eaef991b3c0ad3a101811a35a1
Noah Yek <ninjayek@gmail.com>
2019-11-07 10:17:50 +0800

    test file commit

--- JustRipeFarmUnitTest/UnitTestLabourerHandler.cs
+++ JustRipeFarmUnitTest/UnitTestLabourerHandler.cs
@@ -23,6 +23,7 @@ namespace JustRipeFarmUnitTest
             LabourerHandler labrHand = new LabourerHandler();
             int resp2 = labrHand.addNewLabourer(dbC.getConn(), labrA);
             Assert.IsNotNull(resp2);
+            //Hello i am Noah code
         }
     }
 }
